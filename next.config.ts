f99fb03894bb8f1cc1671b53d80fcc71831edc46
deva <devanand@i2global.io>
2025-12-31 16:17:48 +0530

    env added in next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env:{
+    DAILY_API_KEY: process.env.DAILY_API_KEY,
+  }
 };
 
 export default nextConfig;
